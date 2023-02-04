1a6a94982017c28dd5724ddaca62aa2fb7815016
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2023-02-04 04:22:24 -0500

    crear componente menu

--- appBiblioteca/src/app/app.module.ts
+++ appBiblioteca/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent,
+                 MenuComponent],
+                 
+  imports: [BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule],
+
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
